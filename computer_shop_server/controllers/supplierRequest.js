30ce2ffa16ffe70406b6ebcc492ce149d8af6538
aloneyal08 <aloneyal08@gmail.com>
2024-06-27 15:36:38 +0300

    finish request flow

--- computer_shop_server/controllers/supplierRequest.js
+++ computer_shop_server/controllers/supplierRequest.js
@@ -1,9 +1,17 @@
 const SupplierRequest = require('../models/supplierRequest');
 const User = require('../models/user');
 const { encrypt, decrypt } = require('../utils');
-
-var AWS = require("aws-sdk");
-AWS.config.update({region:'us-east-1'});
+var nodemailer = require('nodemailer');
+require('dotenv').config()
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'computer.shop.colman@gmail.com',
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
 const createRequest = async (req, res) => {
   const { description, username, password, email, phone, fullName } = req.body;
@@ -27,105 +35,124 @@ const createRequest = async (req, res) => {
     description
   });
   await request.save();
-  const admins = await User.find({level: 2}, {email: 1, _id: 0});
-  console.log(admins);
-  var params = {
-    Destination: {
-      ToAddresses: admins.map(admin=>admin.email)
-    },
-    Message: {
-      Body: {
-        Html: {
-        Charset: "UTF-8",
-        Data: `
-          <style>
-            button {
-              display: inline-block;
-              padding: 15px 25px;
-              font-size: 24px;
-              cursor: pointer;
-              text-align: center;
-              text-decoration: none;
-              outline: none;
-              color: #fff;
-              background-color: #04AA6D;
-              border: none;
-              border-radius: 15px;
-              box-shadow: 0 9px #999;
-              margin: 10px;
-            }
-
-            button:hover {background-color: #3e8e41}
-
-            button:active {
-              background-color: #3e8e41;
-              box-shadow: 0 5px #666;
-              transform: translateY(4px);
-            }
-            .rejectButton {
-              background-color: rgb(190, 58, 58);
-            }
-            .rejectButton:hover {
-              background-color: darkred;
-            }
-          
-          </style>
-          <div style="background-image: url(https://us.123rf.com/450wm/panychev/panychev1603/panychev160300672/54290362-abstract-sfondo-blu-scuro.jpg?ver=6); position: absolute;width: 100%;height: 100%;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;background-size: cover;">
-            <div style="background-color: white;position: absolute;width: 85%;height: 85%;border-radius: 44px;left: 50%;top: 50%;transform: translate(-50%, -50%);">
-              <h1 style="font-size: 50px;text-align: center;">Hello, ${fullName} sent a request to be a supplier.</h1>
-              <p style="text-align: center;font-size: 20px;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;margin: 30px;">
-                ${description}
-              </p>
-              <div style="margin: auto;display: flex;justify-content: center;">
-                <button onclick='accept()'>Accept</button>
-                <button onclick='reject()' class="rejectButton">Reject</button>
-              </div>
+  const admins = await User.find({level: 2});
+  const obj =  Buffer.from(encrypt(JSON.stringify({id: request._id, username: admins[0].username, password: admins[0].password})), 'utf8').toString('base64');
+  const cancelObj = Buffer.from(encrypt(JSON.stringify({id: request._id, password})), 'utf8').toString('base64');
+  var mailOptions = {
+    from: 'computer.shop.colman@gmail.com',
+    to: admins.map(admin=>admin.email).join(", "),
+    subject: 'Supplier Request',
+    html: `
+        <div style="background-image: url(https://us.123rf.com/450wm/panychev/panychev1603/panychev160300672/54290362-abstract-sfondo-blu-scuro.jpg?ver=6); position: absolute;width: 100%;height: 100%;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;background-size: cover;display: flex;justify-content: center;">
+          <div style="background-color: white;position: absolute;width: 85%;height: 85%;border-radius: 44px;left: 50%;top: 50%;transform: translate(-50%, -50%);">
+            <h1 style="font-size: 40px;text-align: center;">Hello, ${fullName} sent a request to be a supplier.</h1>
+            <p style="text-align: center;font-size: 20px;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;margin: 30px;">
+              ${description}
+            </p>
+            <div style="margin: auto;display: flex;justify-content: center;padding: auto;">
+              <a href="http://localhost:88/supplier/request/accept?obj=${obj}"><button style='display: inline-block;padding: 15px 25px;font-size: 24px;cursor: pointer;text-align: center;text-decoration: none;outline: none;color: #fff;background-color: #04AA6D;border: none;border-radius: 15px;box-shadow: 0 9px #999;margin: 10px;'>Accept</button></a>
+              <a href="http://localhost:88/supplier/request/reject?obj=${obj}"><button style='display: inline-block;padding: 15px 25px;font-size: 24px;cursor: pointer;text-align: center;text-decoration: none;outline: none;color: #fff;background-color: rgb(190, 58, 58);border: none;border-radius: 15px;box-shadow: 0 9px #999;margin: 10px;'>Reject</button></a>
             </div>
           </div>
-          <script>
-            const accept = () => {
-              fetch('http://localhost:3000/supplier/request/accept', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  id: '${email}',
-                }),
-              })
-            }
-            const reject = () => {
-              fetch('http://localhost:3000/supplier/request/reject', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  id: '${email}',
-                }),
-              })
-            }
-          </script>
-        `
-        },
-      },
-      Subject: {Charset: 'UTF-8',Data: 'Supplier Request: ' + fullName}
-    },
-    Source: 'computer.shop.colman@gmail.com',
-    ReplyToAddresses: ['computer.shop.colman@gmail.com',],
-  };
-  var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-
-  sendPromise.then(function(data) {
-    res.json("true")
-  }).catch(
-    function(err) {
-    console.error(err, err.stack);
+      `
+  } 
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  }); 
+  mailOptions = {
+    from: 'computer.shop.colman@gmail.com',
+    to: email,
+    subject: 'Supplier Request',
+    html: `
+        <div style="background-image: url(https://us.123rf.com/450wm/panychev/panychev1603/panychev160300672/54290362-abstract-sfondo-blu-scuro.jpg?ver=6); position: absolute;width: 100%;height: 100%;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;background-size: cover;">
+          <div style="background-color: white;position: absolute;width: 85%;height: 85%;border-radius: 44px;left: 50%;top: 50%;transform: translate(-50%, -50%);">
+            <h1 style="font-size: 40px;text-align: center;">Hello,you sent a request to become a supplier for our shop!</h1>
+            <p style="text-align: center;font-size: 20px;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;margin: 30px;">
+              if you did not make the request or wish to cancel it, press the link: <a href='http://localhost:88/supplier/request/cancel?obj=${cancelObj}'>cancel</a>
+              <br/>Thank you for using our site!
+            </p>
+          </div>
+      `
+  } 
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
   });
-
   res.status(201).json(request);
 }
 
+const acceptRequest = async (req, res) => {
+  const { obj } = req.query;
+  var parsed;
+  try {
+    parsed = JSON.parse(decrypt(Buffer.from(obj, 'base64').toString('utf8')));
+  } catch(error) {
+    return res.status(400).json({error: 'Invalid Request'});
+  }
+  const { id, username, password } = parsed;
+
+  const admin = await User.findOne({username});
+  if(!admin || admin.level !== 2 || admin.password !== password)
+    return res.status(400).json({error: 'Invalid Login'});
+
+  const request = await SupplierRequest.findByIdAndDelete(id);
+  if(!request) {
+    return res.status(400).json({error: 'Invalid Request'});
+  }
+
+  const user = new User({...request.user})
+  await user.save();
+  res.status(201).json({message: 'Request accepted'});
+}
+
+const rejectRequest = async (req, res) => {
+  const { obj } = req.query;
+  var parsed;
+  try {
+    parsed = JSON.parse(decrypt(Buffer.from(obj, 'base64').toString('utf8')));
+  } catch(error) {
+    return res.status(400).json({error: 'Invalid Request'});
+  }
+  const { id, username, password } = parsed;
+
+  const admin = await User.findOne({username});
+  if(!admin || admin.level !== 2 || admin.password !== password)
+    return res.status(400).json({error: 'Invalid Login'});
+
+  const request = await SupplierRequest.findByIdAndDelete(id);
+
+  if(!request)
+    return res.status(400).json({error: 'Request not found'});
+  res.status(201).json({message: 'Request rejected'});
+}
+
+const cancelRequest = async (req, res) => {
+  const { obj } = req.query;
+  var parsed;
+  try {
+    parsed = JSON.parse(decrypt(Buffer.from(obj, 'base64').toString('utf8')));
+  } catch(error) {
+    return res.status(400).json({error: 'Invalid Request'});
+  }
+  const { id, password } = parsed;
+  const request = await SupplierRequest.findById(id);
+  if(!request || decrypt(request.user.password) !== password)
+    return res.status(400).json({error: 'Invalid Login'});
+
+  await SupplierRequest.findByIdAndDelete(id);
+  res.status(201).json({message: 'Request canceled'});
+}
+
 module.exports = {
-  createRequest
+  createRequest,
+  acceptRequest,
+  rejectRequest,
+  cancelRequest
 };
--- computer_shop_server/routers/supplierRequest.js
+++ computer_shop_server/routers/supplierRequest.js
@@ -4,5 +4,8 @@ var router = express.Router();
 const SupplierRequestController = require('../controllers/supplierRequest');
 
 router.post('/create', SupplierRequestController.createRequest);
+router.get('/accept', SupplierRequestController.acceptRequest);
+router.get('/reject', SupplierRequestController.rejectRequest);
+router.get('/cancel', SupplierRequestController.cancelRequest);
 
 module.exports = router;
