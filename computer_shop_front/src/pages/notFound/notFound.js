5b24b4afb1b863511bb4e3d031018afb0556232c
eyalgonlod <eyaleigi@gmail.com>
2024-07-18 16:18:40 +0200

    Not_found_Design

--- computer_shop_front/src/pages/notFound/notFound.js
+++ computer_shop_front/src/pages/notFound/notFound.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import './notFound.css'
+import notFoundImage from '../../images/404.png';
+
 
 export const NotFound = () => {
-  return <div>
-    <h1>Not Found</h1>
-  </div>
+  return (
+    <div className="container">
+      <img src={notFoundImage} alt="404 Not Found" id="notFoundImage" />
+      <p id='first_for_notFound'>Oops! The page you are looking for does not exist.</p>
+      <p id='second_for_notFound'>Please Make Sure You Wrote The Right Url!</p>
+      <a href="/" className="home-button">Go Home</a>
+    </div>
+  );
 }
