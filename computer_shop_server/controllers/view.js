57cf4b2f15e6604e3acba83d9b0bc97eb10ce38f
aloneyal08 <aloneyal08@gmail.com>
2024-07-29 21:11:02 +0300

    fix view bug

--- computer_shop_server/controllers/view.js
+++ computer_shop_server/controllers/view.js
@@ -6,13 +6,21 @@ const AddView = async (req, res) => {
 	const { user, product } = req.body;
 	if(!mongoose.Types.ObjectId.isValid(product))
 		return res.status(404).json({error: "couldn't get product"});
-  const p = await Product.findById(product);
-  if(!p){
-    return res.status(400).json({error: 'Product not found'});
+
+	const d = new Date();
+	d.setHours(0,0,0,0);
+	const views = await View.find({user: user}).sort({date: -1}).limit(1)
+
+	if(views.length===0 || views[0].date < d) {
+		const p = await Product.findById(product);
+		if(!p){
+			return res.status(400).json({error: 'Product not found'});
+		}
+		const view = new View({ user, product });
+		await view.save();
+		return res.status(200).json(view);
 	}
-	const view = new View({ user, product });
-	await view.save();
-	res.status(200).json(view);
+	return res.status(200).json({})
 }
 
 const GetViews = async (req, res) => {
